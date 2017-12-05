6793e4e1fa54173e9b2e3cb2e7a9a18e1098d0b9
Marco Montilla <marco.jr1695@gmail.com>
2017-12-05 16:39:19 -0400

    Active acount client

---
+++ active.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+session_unset();
+
+require_once "./src/app_api/config/connection.php";
+require_once "./src/app_api/modules/methods/client.php";
+
+$id = $_GET["x"];
+$salt = $_GET["y"];
+
+$obj = new Methods();
+$resp = $obj->checkAcount('client',$id, $salt);
+
+if ($resp['active'] == '1') {
+  header('Location: http://comiczone.hol.es/');
+  echo 'alert("esta activo")';
+} else {
+  $obj = new Methods();
+  $resp = $obj->confirmAcount('client',$id, $salt);
+  if ($resp) {
+    header('Location: http://comiczone.hol.es/');
+  } else {
+    echo 'error';
+  }
+
+}
